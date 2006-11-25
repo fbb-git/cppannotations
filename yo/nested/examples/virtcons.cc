c82ad5f36baf7dbeba3f2410e8b5da8d977bc0cb
fbbrokken <fbbrokken>
2006-11-25 15:38:52 +0000

    Moved more member implementations to below their interfaces
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@42 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/nested/examples/virtcons.cc
+++ yo/nested/examples/virtcons.cc
@@ -8,8 +8,7 @@
             class Base
             {
                 public:
-                    virtual ~Base()
-                    {}
+                    virtual ~Base();
                     virtual Base *clone() const = 0;
             };
 
@@ -17,59 +16,77 @@
             Base *d_bp;
 
         public:
-            Clonable()
-            :
-                d_bp(0)
-            {}
-            ~Clonable()
-            {
-                delete d_bp;
-            }
-            Clonable(Clonable const &other)
-            {
-                copy(other);
-            }
-            Clonable &operator=(Clonable const &other)
-            {
-                if (this != &other)
-                {
-                    delete d_bp;
-                    copy(other);
-                }
-                return *this;
-            }
+            Clonable();
+            ~Clonable();
+            Clonable(Clonable const &other);
+            Clonable &operator=(Clonable const &other);
 
             // New for virtual constructions:
-            Clonable(Base const &bp)
-            {
-                d_bp = bp.clone();      // allows initialization from
-            }                           // Base and derived objects
-            Base &get() const
-            {
-                return *d_bp;
-            }
+            Clonable(Base const &bp);
+            Base &get() const;
 
         private:
-            void copy(Clonable const &other)
-            {
-                if ((d_bp = other.d_bp))
-                    d_bp = d_bp->clone();
-            }
+            void copy(Clonable const &other);
     };
+
+
+    inline Clonable::Base::~Base()
+    {}
+    
+    inline Clonable::Clonable()
+    :
+        d_bp(0)
+    {}
+    inline Clonable::~Clonable()
+    {
+        delete d_bp;
+    }
+    inline Clonable::Clonable(Clonable const &other)
+    {
+        copy(other);
+    }
+    inline Clonable &Clonable::operator=(Clonable const &other)
+    {
+        if (this != &other)
+        {
+            delete d_bp;
+            copy(other);
+        }
+        return *this;
+    }
+    
+    inline Clonable::Clonable(Base const &bp)
+    {
+        d_bp = bp.clone();      // allows initialization from
+    }                           // Base and derived objects
+    
+    inline Clonable::Base &Clonable::get() const
+    {
+        return *d_bp;
+    }
+    
+    inline void Clonable::copy(Clonable const &other)
+    {
+        if ((d_bp = other.d_bp))
+            d_bp = d_bp->clone();
+    }
 
     class Derived1: public Clonable::Base
     {
         public:
-            ~Derived1()
-            {
-                std::cout << "~Derived1() called\n";
-            }
-            virtual Clonable::Base *clone() const
-            {
-                return new Derived1(*this);
-            }
+            ~Derived1();
+            virtual Clonable::Base *clone() const;
     };
 
+    inline Derived1::~Derived1()
+    {
+        std::cout << "~Derived1() called\n";
+    }
+    inline Clonable::Base *Derived1::clone() const
+    {
+        return new Derived1(*this);
+    }
+    
     using namespace std;
 
     int main()
