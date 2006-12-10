8ee67f2c22af2b5afed76a6d48a8dc75170ef596
danchev <danchev>
2006-12-10 12:08:31 +0000

    fix abnormal abort of terminate called after throwing an instance of std::logic_error when no args were being given
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@47 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/string/cc/stringreplace.cc
+++ yo/string/cc/stringreplace.cc
@@ -5,7 +5,7 @@
 
     int main(int argc, char **argv)
     {
-        if (argc == 3)
+        if (argc != 3)
         {
             cerr << "Usage: <searchstring> <replacestring> to process "
                                                                   "stdin\n";
