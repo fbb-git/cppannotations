e6910b96f61e0b6ea373f80fe9e1b38187bc9f5e
fbbrokken <fbbrokken>
2007-05-01 21:57:59 +0000

    added the inheritance/nondefault section
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@54 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/inheritance/examples/nstrings.cc
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+string *nStrings(size_t size, char const *fname)
+{
+    static ifstream in;
+
+    struct Xstr: public string
+    {
+        Xstr()
+        :
+            string(nextLine())
+        {}
+        static char const *nextLine()
+        {
+            static string line;
+
+            getline(in, line);
+            return line.c_str();
+        }
+    };
+    in.open(fname);
+    string *sp = new Xstr[10];
+    in.close();
+
+    return sp;
+}
+
+int main()
+{
+    string *sp = nStrings(10, "nstrings.cc");
+    copy(sp, sp + 10, ostream_iterator<string>(cout, "\n"));
+}
