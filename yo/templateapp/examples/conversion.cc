991a5aa96da08a38d3b64b8eff249e19a4950cbb
fbbrokken <fbbrokken>
2007-06-13 14:56:41 +0000

    First version of the Advanced Template Chapter completed
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@93 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/templateapp/examples/conversion.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "conversion.h"
+
+using namespace std;
+
+int main()
+{
+//CONVERSION
+    cout << 
+        Conversion<ofstream, ostream>::exists << " " <<
+        Conversion<ostream, ofstream>::exists << " " <<
+        Conversion<int, double>::exists << " " <<
+        Conversion<int, string>::exists << " " << 
+        endl;
+//=
+
+    cout << "\n" <<
+        Conversion<int, int>::sameType << " " << 
+        Conversion<int, string>::sameType << " " << 
+        endl;
+
+//INHERITANCE
+    cout << "\n" <<
+        "1: " << BASE_1st_DERIVED_2nd(ofstream, ostream) << ",  " <<
+        "2: " << BASE_1st_DERIVED_2nd(ostream, ofstream) << ",  " <<
+        "3: " << BASE_1st_DERIVED_2nd(void, ofstream) << ",  " <<
+        "4: " << BASE_1st_DERIVED_2nd(ostream, ostream) << ",  " <<
+        "5: " << BASE_1st_DERIVED_2nd_STRICT(ostream, ostream) << " " <<
+        endl;
+//=
+}
