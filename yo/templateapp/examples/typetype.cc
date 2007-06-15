93f0b4d9038d4a4b8f9644de80036c9116c9d9e6
fbbrokken <fbbrokken>
2007-06-15 10:02:45 +0000

    Finalized templateapp/typetype.yo
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@100 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/templateapp/examples/typetype.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+#include "typetype.h"
+
+
+    template <typename Return, typename Argument>
+    Return chop(Argument const &arg, TypeType<Argument> )
+    {
+        return Return(arg);
+    }
+
+    template <typename Argument>
+    std::string chop(Argument const &arg, TypeType<std::string> )
+    {
+        return std::string(arg, 1);
+    }
+
+    template <typename Result>
+    Result chopper(char const *txt)
+    {
+        return chop(std::string(txt), TypeType<Result>());
+    }
+
+    
+    using namespace std;
+
+    int main()
+    {
+        cout << chopper<string>("hello world") << endl;
+    }
+
+
+
+
+
