857a0fca9faadd353e1ab5a99bc4c30dbdc1c1c1
Frank B. Brokken <f.b.brokken@rug.nl>
2009-06-28 18:10:51 +0000

    WIP on move constructors etc.
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@208 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/memory/examples/intarraymove.cc
@@ -0,0 +1,28 @@
+    #include <cstddef>
+
+    class IntArray
+    {
+        int *d_data;
+        size_t d_capacity;
+        size_t d_nUsed;
+
+        public:
+            IntArray(size_t capacity = 0);
+            IntArray(IntArray const &other);
+            IntArray(IntArray &&other);             // move constructor
+            ~IntArray();
+
+            IntArray &operator=(IntArray const &other);
+            IntArray &operator=(IntArray &&other);  // Note: move operator=()
+    };
+        
+    IntArray::IntArray(IntArray &&other)
+    {
+        d_data = other.d_data;
+        other.d_data = 0;
+        d_capacity = other.d_capacity;
+        d_nUsed = other.d_nUsed;
+    }
+
+
+
