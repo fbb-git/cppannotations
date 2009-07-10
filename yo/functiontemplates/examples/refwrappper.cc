9a8bcc0e9d4803c13ca7d6a335b600bb5400f395
Frank B. Brokken <f.b.brokken@rug.nl>
2009-07-10 08:43:38 +0000

    added section about reference wrappers
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@222 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/functiontemplates/examples/refwrappper.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <functional>
+
+using namespace std;
+
+    // BEFORE COMPILING THIS SOURCE: COMMENT OUT `outer(arg);' BELOW
+
+//INT
+    template <typename Type>
+    void outer(Type t)
+    {
+        t.x();
+    }
+
+    void useInt()
+    {
+        int arg;
+        outer(arg);
+    }
+//=
+
+
+//DOUBLE
+    void sqrtArg(double &arg)
+    {
+        arg = sqrt(arg);
+    }
+
+    template<typename Fun, typename Arg>    
+    void call(Fun fun, Arg arg)
+    {
+        fun(arg);
+        cout << "In call: arg = " << arg << endl;
+    }
+//=
+
+//MAIN
+    int main()
+    {
+        double value = 3;
+        call(sqrtArg, value);
+        cout << "Passed value, returns: " << value << endl;
+    
+        call(sqrtArg, ref(value));
+        cout << "Passed ref(value), returns: " << value << endl;
+    }
+    /*
+        Output of this program:
+
+        In call: arg = 1.73205
+        Passed value, returns: 3
+        In call: arg = 1.73205
+        Passed ref(value), returns: 1.73205
+    */
+//=
