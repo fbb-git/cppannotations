23303c1c4fe6337937fe285f97481e6ae56bb09c
Frank B. Brokken <f.b.brokken@rug.nl>
2009-07-15 15:52:54 +0000

    Flaws removed
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@232 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/stl/examples/move.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+    struct Data
+    {
+        int d_value;
+
+        Data(int value)
+        :
+            d_value(value)
+        {
+            cout << "cons "<< d_value << "\n";
+        }
+        Data(Data const &other)
+        :
+            d_value(other.d_value)
+        {
+            cout << "copy " << d_value << endl;
+        }
+        Data(Data &&other)
+        :
+            d_value(other.d_value)
+        {
+            cout << "move " << other.d_value << " to " << d_value << endl;
+        }
+    };
+
+    Data &&makeData(int x)
+    {
+        Data ret(x);
+        return move(ret);
+    }
+
+    int main()
+    {
+        Data &&ref = makeData(1);
+        cout << ref.d_value << endl;
+
+        return 0;
+    }
+    /* 
+        Output with g++ 4.3.3:
+            cons 1
+            move 134514832 to 134514832
+            134514832
+
+        Output with g++ 4.4.0:
+            cons 1
+            move 1 to 1
+            1
+    */
+
