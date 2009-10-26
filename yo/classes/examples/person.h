ecc95ef6814e3810e62da29482c579a6e6018fb0
Frank B. Brokken <f.b.brokken@rug.nl>
2009-10-26 12:06:52 +0000

    WIP on classes
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@269 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/classes/examples/person.h
+++ yo/classes/examples/person.h
@@ -2,19 +2,19 @@
 
     class Person
     {
-        std::string d_name;     // name of person
-        std::string d_address;  // address field
-        std::string d_phone;    // telephone number
-        size_t    d_weight;   // the weight in kg.
+        std::string d_name;         // name of person
+        std::string d_address;      // address field
+        std::string d_phone;        // telephone number
+        size_t      d_weight;       // the weight in kg.
 
-        public:                 // interface functions
-            void setName(std::string const &n);
-            void setAddress(std::string const &a);
-            void setPhone(std::string const &p);
+        public:                     // member functions
+            void setName(std::string const &name);
+            void setAddress(std::string const &address);
+            void setPhone(std::string const &phone);
             void setWeight(size_t weight);
 
             std::string const &name()    const;
             std::string const &address() const;
             std::string const &phone()   const;
-            size_t weight()            const;
+            size_t weight()              const;
     };
