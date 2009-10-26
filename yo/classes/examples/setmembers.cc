214f0ec194415c14ec191d577e994148dcb18fdb
Frank B. Brokken <f.b.brokken@rug.nl>
2009-10-26 16:01:51 +0000

    WIP on classes
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@270 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/classes/examples/setmembers.cc
+++ yo/classes/examples/setmembers.cc
@@ -1,21 +1,17 @@
     #include "person.h"                 // given earlier
 
-    // interface functions set...()
     void Person::setName(string const &name)
     {
         d_name = name;
     }
-
     void Person::setAddress(string const &address)
     {
         d_address = address;
     }
-
     void Person::setPhone(string const &phone)
     {
         d_phone = phone;
     }
-
     void Person::setWeight(size_t weight)
     {
         d_weight = weight;
