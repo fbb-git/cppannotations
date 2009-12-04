d522038c37e0aef6ef2266431ece5570c454c1bf
Frank B. Brokken <f.b.brokken@rug.nl>
2009-12-04 19:28:24 +0000

    WIP
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@313 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/overloading/examples/stringconversionerror.cc
+++ yo/overloading/examples/stringconversionerror.cc
@@ -2,15 +2,51 @@
     #include <string>
     using namespace std;
 
-    class NoInsertion
+    struct Insertable
     {
-        public:
-            operator string() const;
+        operator int() const
+        {
+            cout << "op int()\n";
+        }
+    };
+    ostream &operator<<(ostream &out, Insertable const &ins)
+    {
+        return out << "insertion operator";
+    }
+    struct Convertor
+    {
+        operator Insertable() const
+        {
+            return Insertable();
+        }
+    };
+    struct Text
+    {
+        operator int() const
+        {
+            return 1;
+        }
+    };
+    struct Error
+    {
+        operator Text() const
+        {
+            return Text();
+        }
     };
 
     int main()
     {
-        NoInsertion object;
-
-        cout << object << endl;
+        Insertable insertable;
+        cout << insertable << '\n';
+        Convertor convertor;
+        cout << convertor << '\n';
+        Error error;
+        cout << error << '\n';
     }
+
+
+
+
+
+
