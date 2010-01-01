aafbfe4ca21a431050977e35135ab7e694ff5409
Frank B. Brokken <f.b.brokken@rug.nl>
2010-01-01 10:14:14 +0000

    Generic algoritms completed, function templates next
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@370 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/generic/examples/heap.cc
+++ yo/generic/examples/heap.cc
@@ -2,16 +2,14 @@
     #include <iostream>
     #include <functional>
     #include <iterator>
+    using namespace std;
 
     void show(int *ia, char const *header)
     {
-        std::cout << header << ":\n";
-        std::copy(ia, ia + 20, std::ostream_iterator<int>(std::cout, " "));
-        std::cout << '\n';
+        cout << header << ":\n";
+        copy(ia, ia + 20, ostream_iterator<int>(cout, " "));
+        cout << '\n';
     }
-
-    using namespace std;
-
     int main()
     {
         int ia[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -41,26 +39,23 @@
 
         sort_heap(ia, ia + 20, greater<int>());
         show(ia, "Sorting the elements in the heap");
-
-        return 0;
     }
     /*
         Displays:
-
-        The values 1-20 in a max-heap:
-        20 19 15 18 11 13 14 17 9 10 2 12 6 3 7 16 8 4 1 5
-        Removing the first element (now at the end):
-        19 18 15 17 11 13 14 16 9 10 2 12 6 3 7 5 8 4 1 20
-        Adding 20 (at the end) to the heap again:
-        20 19 15 17 18 13 14 16 9 11 2 12 6 3 7 5 8 4 1 10
-        Sorting the elements in the heap:
-        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-        The values 1-20 in a heap, using > (and beyond too):
-        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-        Removing the first element (now at the end):
-        2 4 3 8 5 6 7 16 9 10 11 12 13 14 15 20 17 18 19 1
-        Re-adding the removed element:
-        1 2 3 8 4 6 7 16 9 5 11 12 13 14 15 20 17 18 19 10
-        Sorting the elements in the heap:
-        20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+            The values 1-20 in a max-heap:
+            20 19 15 18 11 13 14 17 9 10 2 12 6 3 7 16 8 4 1 5
+            Removing the first element (now at the end):
+            19 18 15 17 11 13 14 16 9 10 2 12 6 3 7 5 8 4 1 20
+            Adding 20 (at the end) to the heap again:
+            20 19 15 17 18 13 14 16 9 11 2 12 6 3 7 5 8 4 1 10
+            Sorting the elements in the heap:
+            1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+            The values 1-20 in a heap, using > (and beyond too):
+            1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+            Removing the first element (now at the end):
+            2 4 3 8 5 6 7 16 9 10 11 12 13 14 15 20 17 18 19 1
+            Re-adding the removed element:
+            1 2 3 8 4 6 7 16 9 5 11 12 13 14 15 20 17 18 19 10
+            Sorting the elements in the heap:
+            20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
     */
