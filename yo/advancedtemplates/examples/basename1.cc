bc732603bd23f2508da7447a6a71c5559ea21e1f
Frank B. Brokken <f.b.brokken@rug.nl>
2010-02-09 14:56:47 +0000

    WIP
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@388 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/advancedtemplates/examples/basename1.cc
+++ yo/advancedtemplates/examples/basename1.cc
@@ -6,20 +6,17 @@
         public:
             void member();
     };
-
     template <typename T>
     void Base<T>::member()
     {
         std::cout << "This is Base<T>::member()\n";
     }
-
     template <typename T>
     class Derived: public Base<T>
     {
         public:
             Derived();
     };
-
     template <typename T>
     Derived<T>::Derived()
     {
--- yo/advancedtemplates/examples/basename2.cc
+++ yo/advancedtemplates/examples/basename2.cc
@@ -6,33 +6,29 @@
         public:
             void member();
     };
-
-        template <typename T>
-        void Base<T>::member()
-        {
-            std::cout << "This is Base<T>::member()\n";
-        }
-
-        template <>
-        void Base<int>::member()
-        {
-            std::cout << "This is the int-specialization\n";
-        }
-
+    template <typename T>
+    void Base<T>::member()
+    {
+        std::cout << "This is Base<T>::member()\n";
+    }
+    template <>
+    void Base<int>::member()
+    {
+        std::cout << "This is the int-specialization\n";
+    }
     template <typename T>
     class Derived: public Base<T>
     {
         public:
             Derived();
     };
-
-        template <typename T>
-        Derived<T>::Derived()
-        {
-            this->member();         // Using `this' implies <T> at
-                                    // instantiation time.
-            Base<T>::member();      // Same.
-        }
+    template <typename T>
+    Derived<T>::Derived()
+    {
+        this->member();         // Using `this' implies <T> at
+                                // instantiation time.
+        Base<T>::member();      // Same.
+    }
 
     int main()
     {
