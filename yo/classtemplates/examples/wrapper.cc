75866d2f7b2fc1b632edd8c19e4c2d9a9792f7e3
Frank B. Brokken <f.b.brokken@rug.nl>
2010-02-11 20:45:06 +0000

    wrapper generator in wrapper.cc operational
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@394 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/classtemplates/examples/wrapper.cc
+++ yo/classtemplates/examples/wrapper.cc
@@ -3,8 +3,6 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 // The Aux struct receives 
 //  1. an index that reduces to zero initially representing the number of
 //      elements in a tuple (int t_size); 
@@ -14,27 +12,30 @@ using namespace std;
 //     function call operator (arg, typename ArgType))
 //  5. Any other series of arguments (params, typename ... Params)
 
+namespace FBB
+{
+
+namespace IUO
+{
+
 template <  int size, 
             typename Tuple,
             typename Fun,
             typename ArgType,
             typename ... Params>
-struct Aux
+struct Wrap
 {
-    typedef typename tuple_element<size - 1, Tuple>::type ElType;
+    typedef typename std::tuple_element<size - 1, Tuple>::type ElType;
 
-//    inline Aux()
-//    {}
-
-    inline Aux(Tuple &tuple, Fun fun, ArgType &&arg, Params && ... params)
+    inline Wrap(Tuple &&tuple, Fun fun, ArgType &&arg, Params && ... params)
     {
-        Aux<size - 1, Tuple, Fun, ArgType, ElType, Params ...>
+        Wrap<size - 1, Tuple, Fun, ArgType, ElType, Params ...>
         (
-            tuple, 
+            std::forward<Tuple>(tuple), 
             fun,
-            forward<ArgType>(arg),
-            forward<ElType>(get<size - 1>(tuple)), 
-            forward<Params>(params) ...);
+            std::forward<ArgType>(arg),
+            std::forward<ElType>(std::get<size - 1>(tuple)), 
+            std::forward<Params>(params) ...);
     }
 };
     
@@ -47,29 +48,31 @@ template <
             typename Fun,
             typename ArgType,
             typename ... Params>
-struct Aux<0, Tuple, Fun, ArgType, Params ...>
+struct Wrap<0, Tuple, Fun, ArgType, Params ...>
 {
-    inline Aux(Tuple &tuple, Fun fun, ArgType &&arg, Params && ... params)
+    inline Wrap(Tuple &&tuple, Fun fun, ArgType &&arg, Params && ... params)
     {
-        fun(forward<ArgType>(arg), forward<Params>(params) ...);
+        fun(std::forward<ArgType>(arg), std::forward<Params>(params) ...);
     }
 };
 
+} // IUO
+
 template <typename Fun, typename ... Params>
-class Wrapper
+class Wrap
 {
     template <  int size, 
             typename Tuple, 
             typename Fn,
             typename ArgType,
             typename ... Parms>
-    friend struct Aux;
+    friend struct IUO::Wrap;
 
     Fun d_fun;
     std::tuple<Params ...> d_tuple;
     
     public:
-        Wrapper(Fun fun, Params && ... params)
+        Wrap(Fun fun, Params && ... params)
         :
             d_fun(fun),
             d_tuple(std::forward<Params>(params) ...)
@@ -78,24 +81,37 @@ class Wrapper
         template <typename ArgType>
         void operator()(ArgType &&param)
         {
-            Aux<tuple_size<tuple<Params ...>>::value, 
-                tuple<Params ...>, 
+            IUO::Wrap<std::tuple_size<std::tuple<Params ...>>::value, 
+                std::tuple<Params ...>, 
                 Fun, 
                 ArgType                
                 >
                 (d_tuple, d_fun, param);
         }
+
 };
+
+struct Wrapper
+{
+    template <typename Func, typename ... Param>
+    static Wrap<Func, Param ...> 
+        gen(Func fun, Param && ... param)
+        {
+            Wrap<Func, Param ...> 
+                ret(fun, std::forward<Param>(param) ... );
+            return ret;
+        }
+};
+
+} // FBB
+
+using namespace std;
+
+using namespace FBB;
 
 class Vector
 {
     vector<int> d_v;
-
-    template <typename Fun, typename ... Param>
-    static Wrapper<Fun, Param ...> genWrapper(Fun fun, Param && ... param)
-    {
-        return Wrapper<Fun, Param ...>(fun, std::forward<Param>(param) ... );
-    }
 
     public:
 
@@ -108,16 +124,11 @@ class Vector
     {
         int count = 0;
         for_each(d_v.begin(), d_v.end(), 
-            genWrapper(callfun, count, out));
-
-//            Wrapper<
-//        void (*)(int, int &, ostream &), int &, ostream &>
-//                        (callfun, count, out));
+            Wrapper::gen(callfun, count, out));
         cout << "Number of calls: " << count << '\n';
     }
 
     private: 
-
 
     static void callfun(int v, int &count, ostream &o)
     {
