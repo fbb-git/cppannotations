3238d67b8bb7079ad22d246914ae5f0e84a00897
Frank B. Brokken <f.b.brokken@rug.nl>
2010-02-19 21:31:33 +0000

    WIP
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@404 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/advancedtemplates/examples/typelist.h
+++ yo/advancedtemplates/examples/typelist.h
@@ -1,24 +1,14 @@
 #ifndef INCLUDED_TYPELIST_H_
 #define INCLUDED_TYPELIST_H_
 
-#include "nulltype.h"
-
 //TYPELIST
-    template <typename First,  typename Second>
+    template <typename ... Types>
     struct TypeList
     {
-        typedef First Head;
-        typedef Second Tail;
+        TypeList(TypeList const &) = delete;
+        enum { size = sizeof ... (Types) };
     };
 //=
 
-//MACROS
-#define TYPELIST_1(T1)              TypeList<T1, NullType >
-#define TYPELIST_2(T1, T2)          TypeList<T1, TYPELIST_1(T2) >
-#define TYPELIST_3(T1, T2, T3)      TypeList<T1, TYPELIST_2(T2, T3) >
-#define TYPELIST_4(T1, T2, T3, T4)  TypeList<T1, TYPELIST_3(T2, T3, T4) >
-#define TYPELIST_5(T1, T2, T3, T4, T5)  \
-                                    TypeList<T1, TYPELIST_4(T2, T3, T4, T5) >
-//=
 
 #endif
