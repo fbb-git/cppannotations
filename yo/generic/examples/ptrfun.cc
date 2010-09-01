41b270cab76100936aa2c58bb4038f3e72d189ba
Frank B. Brokken <f.b.brokken@rug.nl>
2010-09-01 13:06:53 +0000

    WIP on ptr_fun
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@472 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/generic/examples/ptrfun.cc
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <iterator>
+#include <iostream>
+
+using namespace std;
+
+inline int stringcasecmp(string const &lhs, string const &rhs)
+{
+    return strcasecmp(lhs.c_str(), rhs.c_str());
+}
+
+int main()
+{
+    string target;
+    cin >> target;
+
+    vector<string> v1;
+    copy(istream_iterator<string>(cin), istream_iterator<string>(),
+        back_inserter(v1));
+
+    auto pos = find_if( 
+                    v1.begin(), v1.end(),
+                    not1( bind2nd(ptr_fun(stringcasecmp), target) )
+               );
+
+    if ( pos != v1.end())  
+       cout <<   "The search for `" << target << "' was successful.\n"
+                 "The next string is: `" << pos[1] << "'.\n";
+}
+
+    // on input:
+    //  VERY   I have existed for years, but very little has changed
+    // the program displays:    
+    //  The search for `VERY' was successful.
+    //  The next string is: `little'.
