9f5d015584fa6f39154e1951f433a0578494dbd0
Frank B. Brokken <f.b.brokken@rug.nl>
2011-05-31 15:07:09 +0000

    Adding binop template description to concrete/
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@552 f6dd340e-d3f9-0310-b409-bdd246841980

---
+++ yo/concrete/examples/binary5.h
@@ -0,0 +1,19 @@
+    #ifndef INCLUDED_BINOPS_H_
+    #define INCLUDED_BINOPS_H_
+
+    #include <utility>
+
+    template <typename Type>
+    Type operator+(Type const &lhs, Type const &rhs)
+    {
+        Type tmp(lhs);
+        return operator+(std::move(tmp), rhs);
+    }
+    
+    template <typename Type>
+    Type operator+(Type &&lhs, Type const &rhs)
+    {
+        return lhs += rhs;
+    }
+
+    #endif
