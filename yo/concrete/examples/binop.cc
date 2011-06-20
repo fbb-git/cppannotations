3ac727c34613ac38c10c02ccc16f618c67457cf5
Frank B. Brokken <f.b.brokken@rug.nl>
2011-06-20 09:35:33 +0000

    Repaired A4 overfull boxes
    git-svn-id: https://cppannotations.svn.sourceforge.net/svnroot/cppannotations/trunk@573 f6dd340e-d3f9-0310-b409-bdd246841980

--- yo/concrete/examples/binop.cc
+++ yo/concrete/examples/binop.cc
@@ -36,7 +36,7 @@ class Class
     public:
         Class() = default;
         Class(Class const &other) = default;
-        Class(int) 
+        Class(int)
         {}
         Class(Class &&tmp)
         {
