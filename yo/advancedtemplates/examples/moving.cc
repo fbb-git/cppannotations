74c64002295507b7299d5bf2ccd64f9cd6281e8f
Frank B. Brokken <f.b.brokken@rug.nl>
2013-01-08 21:16:32 +0100

    Added the noexcept section

---
+++ yo/advancedtemplates/examples/moving.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <type_traits>
+#include <string>
+
+using namespace std;
+
+class Foreign
+{
+    public:
+        Foreign() = default;
+        Foreign(string const &) noexcept
+        {}
+
+        Foreign(Foreign const &other) noexcept
+        {
+            throw 1;
+        }
+};
+
+class String
+{
+    public:
+        String() = default;
+        String(String &&tmp)
+        {}
+};
+
+template <typename Type>
+class MyClass
+{
+    Type d_f;
+    public:
+        MyClass() = default;
+        MyClass(MyClass &&tmp)
+        :
+            d_f(move(tmp.d_f))
+        {}
+};
+
+int main()
+try
+{
+    cout << "TRAIT: " << is_nothrow_copy_constructible<Foreign>::value << '\n';
+    MyClass<String> s1(move(MyClass<String>()));
+    MyClass<Foreign> s2(move(MyClass<Foreign>()));
+}
+catch (...)
+{
+    cout << "Exception\n";
+}
+
+
+
+
+
+
+
+
+
