4f181d5f3b1741a76ce5c751b93e280c808ad9f7
Frank B. Brokken <f.b.brokken@rug.nl>
2013-05-24 21:45:58 +0200

    Rewrote stl/thread.yo about the class thread

---
+++ yo/stl/examples/functorthread.cc
@@ -0,0 +1,35 @@
+    #include <iostream>
+    #include <thread>
+    #include <array>
+
+    using namespace std;
+
+    class Functor
+    {
+        array<int, 30> &d_data;
+        int d_value;
+
+        public:
+            Functor(array<int, 30> &data, int value)
+            :
+                d_data(data),
+                d_value(value)
+            {}
+            void operator()(ostream *out)
+            {
+                for (auto &value: d_data)
+                {
+                    value = d_value++;
+                    *out << value << ' ';
+                }
+                *out << '\n';
+            }
+    };
+
+    int main()
+    {
+        array<int, 30> data;
+        Functor functor(data, 5);
+        thread funThread(functor, &cout);
+        funThread.join();
+    };
