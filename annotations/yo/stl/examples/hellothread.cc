36587067cca64ed600bf60f5bbfe5c1a4b8b65c9
Frank B. Brokken <f.b.brokken@rug.nl>
2013-05-29 22:19:25 +0200

    Some fine tuning

--- annotations/yo/stl/examples/hellothread.cc
+++ annotations/yo/stl/examples/hellothread.cc
@@ -1,22 +1,21 @@
-#include <thread>
-#include <iostream>
-#include <unistd.h>
-
-using namespace std;
-
-// do not forget to use -lpthread with g++
-
-void fun(size_t count, char const *txt)
-{
-
-    for (; count--; )
-        cout << txt << endl;
-}
-
-int main()
-{                   // runs the thread following
-                    // the object construction
-    thread display(fun, 3, "hello world");
-
-    display.join(); // see the text
-}
+    #include <thread>
+    #include <iostream>
+    #include <unistd.h>
+    
+    using namespace std;
+    
+    // do not forget to use -pthread with g++
+    
+    void fun(size_t count, char const *txt)
+    {
+    
+        for (; count--; )
+            cout << txt << endl;
+    }
+    int main()
+    {                   // runs the thread following
+                        // the object construction
+        thread display(fun, 3, "hello world");
+    
+        display.join(); // see the text
+    }
--- annotations/yo/stl/examples/threads2.cc
+++ annotations/yo/stl/examples/threads2.cc
@@ -1,26 +1,26 @@
-#include <thread>
-#include <iostream>
-#include <unistd.h>
-
-using namespace std;
-
-// do not forget to use -lpthread with g++
-
-void fun(size_t count, char const *txt)
-{
-
-    for (; count--; )
-        cout << count << ": " << txt << endl;
-}
-
-int main()
-{                   // runs the threads following
-                    // the object construction
-    thread display(fun, 3, "hello world");
-    display.detach();
-
-    thread second(fun, 3, "a second thread");
-    second.detach();
-
-    cout << "leaving" << endl;
-}
+    #include <thread>
+    #include <iostream>
+    #include <unistd.h>
+    
+    using namespace std;
+    
+    // do not forget to use -lpthread with g++
+    
+    void fun(size_t count, char const *txt)
+    {
+    
+        for (; count--; )
+            cout << count << ": " << txt << endl;
+    }
+    
+    int main()
+    {                   // runs the threads following
+                        // the object construction
+        thread display(fun, 3, "hello world");
+        display.detach();
+    
+        thread second(fun, 3, "a second thread");
+        second.detach();
+    
+        cout << "leaving" << endl;
+    }
