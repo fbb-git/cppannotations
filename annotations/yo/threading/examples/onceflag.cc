4e083ba4ab2ae45caaa89f79a0f3b42c3f6c9f70
Frank B. Brokken <f.b.brokken@rug.nl>
2014-09-12 21:43:16 +0200

    WIP

---
+++ annotations/yo/threading/examples/onceflag.cc
@@ -0,0 +1,36 @@
+// initializing a (reduced to the essentials) singleton, using std::once_flag.
+// don't forget -pthread
+
+#include <iostream>
+#include <mutex>
+
+struct Single
+{
+    static Single *s_single;
+    static std::once_flag s_resourceFlag;
+
+    static void initialize()
+    {
+        std::cout << "initializing\n";
+        s_single = new Single;
+    }
+
+    static Single *instance()
+    {
+        std::call_once(s_resourceFlag, initialize);
+        return s_single;
+    }
+};
+
+Single *Single::s_single;
+std::once_flag Single::s_resourceFlag;
+
+int main()
+{
+    Single::instance();
+    Single::instance();
+}
+
+
+
+
