65f8c8cbd784239e428b868d0732ecc48fd840b7
Frank B. Brokken <f.b.brokken@rug.nl>
2014-09-15 12:42:25 +0200

    Added call_once description

---
+++ annotations/yo/threading/examples/callonce.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+class Singleton
+{ 
+    static std::once_flag s_once;
+    static Singleton *s_singleton;
+
+    public:
+        static Singleton *instance()
+        {
+            std::call_once(s_once, []{s_singleton = new Singleton;});
+            return s_singleton;
+        }
+};
+
+Singleton *Singleton::s_singleton = 0;
+std::once_flag Singleton::s_once;
+
+int main()
+{
+    cout << Singleton::instance() << '\n';
+}
