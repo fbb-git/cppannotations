155413df8576e6a715c48fbcdabccad334beccbe
Frank B. Brokken <f.b.brokken@rug.nl>
2014-09-15 11:34:34 +0200

    added section about threadlocal

---
+++ annotations/yo/threading/examples/threadlocal.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+thread_local int t_value = 100;
+
+void modify(char const *label, int newValue)
+{
+    cout << label << " before: " << t_value << ". Address: " << 
+                                                    &t_value << endl;
+    t_value = newValue;
+    cout << label << " after: " << t_value << endl;
+}
+
+int main()
+{
+    thread(modify, "first", 50).join();
+    thread(modify, "second", 20).join();
+    modify("main", 0);
+}
