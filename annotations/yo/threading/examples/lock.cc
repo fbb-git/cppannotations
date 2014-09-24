3b4a06317e5054d5a026333f820730c3702596b0
Frank B. Brokken <f.b.brokken@rug.nl>
2014-09-24 21:12:05 +0200

    wip: deadlocks.yo done

---
+++ annotations/yo/threading/examples/lock.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+//code
+
+int value;
+mutex valueMutex;
+mutex coutMutex;
+
+void fun1()
+{
+    unique_lock<mutex> lg1(coutMutex, defer_lock);
+    unique_lock<mutex> lg2(valueMutex, defer_lock);
+
+    lock(lg1, lg2);
+
+    cout << "fun 1 locks cout\n";
+    cout << "fun 1 locks value\n";
+}
+
+void fun2()
+{
+    unique_lock<mutex> lg1(coutMutex, defer_lock);
+    unique_lock<mutex> lg2(valueMutex, defer_lock);
+
+    lock(lg2, lg1);
+
+    cout << "fun 2 locks cout\n";
+    cout << "fun 2 locks value\n";
+}
+
+int main()
+{
+    thread t1(fun1);
+    thread t2(fun2);
+    t1.join();
+    t2.join();
+}
+
+//=
+    
