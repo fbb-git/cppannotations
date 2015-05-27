e62a5f091f990906ce4f7ec07444c9d573ed80f7
Frank B. Brokken <f.b.brokken@rug.nl>
2015-05-27 21:13:13 +0200

    wip on stl/negators

---
+++ annotations/yo/classtemplates/examples/notfn.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <algorithm>
+
+template <typename Fun>
+struct not_fn_
+{
+    Fun const &d_fun;
+
+    not_fn_(Fun const &fun)
+    :
+        d_fun(fun)
+    {}
+
+    template <typename ... ParTypes>
+    bool operator()(ParTypes && ...types)
+    {
+        return not d_fun(std::forward<ParTypes>(types)...);
+    }
+};
+
+template <typename Fun>
+not_fn_<Fun> not_fn(Fun const &fun)
+{
+    return not_fn_<Fun>(fun);
+}
+
+using namespace std;
+using namespace placeholders;
+
+int main()
+{
+    vector<string> vs {"a", "a", "b"};
+    string reftext {"b"};
+
+    cout << 
+        count_if(vs.begin(), vs.end(),                      // OK
+            bind(not_fn(equal_to<string>()), _1, reftext))
+                << '\n';
+    cout << 
+        count_if(vs.begin(), vs.end(), 
+            not_fn(bind(equal_to<string>(), _1, reftext)))
+                << '\n';
+}
+
+
+
+
+
+
+
