4d14002744c2f29c6758f508d67650b13a10249b
Frank B. Brokken <f.b.brokken@rug.nl>
2015-05-27 13:56:45 +0200

    rewrote generic/memberfunctionadaptors.yo

---
+++ annotations/yo/stl/examples/genlambda.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//code
+int main()
+{
+    auto lambda = [](auto lhs, auto rhs)
+                {
+                    return lhs + rhs;
+                };
+
+    vector<int> values {1, 2, 3, 4, 5};
+    vector<string> text {"a", "b", "c", "d", "e"};
+
+    cout << 
+        accumulate(values.begin(), values.end(), 0, lambda) << '\n' <<
+        accumulate(text.begin(), text.end(), string(), lambda) << '\n';
+}
+//=
