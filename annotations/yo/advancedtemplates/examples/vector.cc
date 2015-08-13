1d3999c32a9123595ff543454381982bc182aadd
Frank B. Brokken <f.b.brokken@rug.nl>
2015-08-13 15:51:28 +0200

    added expr. templ. vector examples in adv.templ./examples/vector*

---
+++ annotations/yo/advancedtemplates/examples/vector.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct VI: public vector<int>
+{
+    static size_t const s_max = 10000;
+    VI()
+    :
+        vector<int>(s_max, 1)
+    {}
+
+    VI &operator+=(VI const &rhs)
+    {
+        for (size_t idx = 0; idx != s_max; ++idx)
+            (*this)[idx] += rhs[idx];
+
+        return *this;
+    }
+};
+
+VI operator+(VI const &lhs, VI const &rhs)
+{
+    VI tmp(lhs);
+
+    tmp += rhs;
+    return tmp;
+}
+
+VI &&operator+(VI &&tmp, VI const &rhs)
+{
+    tmp += rhs;
+    return std::move(tmp);
+}
+
+int main(int argc, char **argv)
+{
+    VI a, b, c, d;
+
+    for (size_t idx = 0, count = stoul(argv[1]); idx != count; ++idx)
+        d = a + b + c + d + a + b + c + d;
+
+    cout << d.front() << ", " << d.back() << '\n';
+}
+
+
+
+
+
+
---
+++ annotations/yo/advancedtemplates/examples/vector0.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//regex s_inetAddr(R"(^\s+inet addr:(\S+))");
+
+struct VI: public vector<int>
+{
+    int d_v = 1;
+
+    VI()
+    {
+        cout << "VI default\n";
+    }
+    ~VI()
+    {
+    }
+    VI(VI const &other)
+    :
+        vector<int>(other),
+        d_v(other.d_v)
+    {
+        cout << "VI copycons\n";
+    }
+    VI(VI &&tmp)
+    :
+        vector<int>(std::move(tmp)),
+        d_v(tmp.d_v)
+    {
+        cout << "VI move cons\n";
+    }
+
+    VI &operator=(VI const &other)
+    {
+        VI tmp(other);
+        swap(tmp);
+        return *this;
+    }
+
+    void swap(VI &other)
+    {
+        std::swap(d_v, other.d_v);
+        static_cast<vector<int> &>(*this).swap(other);
+    }
+
+    VI &operator+=(VI const &rhs)
+    {
+        d_v += rhs.d_v;
+        return *this;
+    }
+    VI &operator+=(VI &&tmp)
+    {
+        d_v += tmp.d_v;
+        return *this;
+    }
+};
+
+VI operator+(VI const &lhs, VI const &rhs)
+{
+    VI tmp(lhs);
+
+    cout << "adding, returning new object\n";
+
+    tmp += rhs;
+    return tmp;
+}
+
+VI &&operator+(VI &&tmp, VI const &rhs)
+{
+    cout << "adding, returning available temporary object\n";
+    tmp += rhs;
+    return std::move(tmp);
+}
+
+int main()
+{
+    VI a, b, c, d;
+
+    d = a + b + c + d + a + b + c + d;
+}
