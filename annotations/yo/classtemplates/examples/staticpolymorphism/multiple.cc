7d9d36109ff8cb18812c8a405e3ccf3ad34c707e
Frank B. Brokken <f.b.brokken@rug.nl>
2015-12-10 16:29:31 +0100

    wip on classtemplates/converting.yo

---
+++ annotations/yo/classtemplates/examples/staticpolymorphism/multiple.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Derived>
+class Vehicle
+{
+    public:
+        void mass() 
+        {
+            static_cast<Derived *>(this)->vmass();
+        }
+    
+    // no need to implement vmass here, as it should be implemented
+    // by derived classes.
+};
+
+class Car: public Vehicle<Car>
+{
+    friend void Vehicle<Car>::mass();
+
+    private:
+        void vmass() 
+        {
+            cout << "Car's vmass called\n";
+        }
+};
+
+class Truck: public Car, public Vehicle<Truck>
+{
+    friend void Vehicle<Car>::mass();
+    private:
+        void vmass() 
+        {
+            cout << "Car's vmass called\n";
+        }
+};
+
+int main()
+{
+    Truck{}.mass();
+}
