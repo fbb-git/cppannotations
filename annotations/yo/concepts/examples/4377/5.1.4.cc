c3f4491df78c0514e49a21f69219c8596659520d
Frank B. Brokken <f.b.brokken@rug.nl>
2016-01-22 16:28:22 +0100

    wip

--- annotations/yo/concepts/examples/4377/5.1.4.cc
+++ annotations/yo/concepts/examples/4377/5.1.4.cc
@@ -12,6 +12,18 @@ concept bool C = requires (T t)
                         { t + t ; }
                     ; // <--- NOTE, as in classes
 
+
+template <typename T>
+concept bool C2()
+{
+    return requires (T t) 
+           { 
+                t + t ; 
+           }
+    ; // ends return
+}
+
+
 template <typename T>
 requires C<T>
 T sum(T a, T b)
@@ -19,8 +31,43 @@ T sum(T a, T b)
     return a + b;
 }
 
+template <typename T>
+concept bool RT() 
+{
+    return requires (T t) 
+            {
+                typename T::type;
+                {*t} -> typename T::type const &;
+            };
+}
+
+template <typename T>
+requires C2<T>()
+T add(T a, T b)
+{
+    return a + b;
+}
+
+
+template <class T>
+concept bool Cd()
+{
+    return requires(T t, double d)
+    {
+        d > 0;
+        t / d;
+    };
+}
+
 int main()
 {
 }
+
+
+
+
+
+
+
 
 
