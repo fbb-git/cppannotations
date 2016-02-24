da818c59cefb978564ddc6034bf25e40f5932d01
Frank B. Brokken <f.b.brokken@rug.nl>
2016-02-24 11:45:25 +0100

    added references to the std::literals::string_literals namespace

---
+++ annotations/yo/namespaces/examples/compound.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+struct Namespace
+{
+//    using namespace std;      cannot be used here
+    std::string d_str;
+
+    std::string text() const;
+};
+
+int main()
+{
+    using namespace std;        // but here is OK
+
+    string text;
+    
+    Namespace ns;
+
+    cout << ns.text();
+}
