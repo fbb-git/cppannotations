f6a233f98635201e225bfd9e18eff739f452b574
Frank B. Brokken <f.b.brokken@rug.nl>
2017-02-09 14:10:02 +0100

    Rewrote the section about adding binary operators

--- annotations/yo/concrete/examples/binopclasses.cc
+++ annotations/yo/concrete/examples/binopclasses.cc
@@ -1,216 +1,85 @@
-// The classes Add and Sub performing the basic arithmetic operations.
-// Each operation type has its own class. Here Add and Sub only provide
-// operator@=  (for demonstration purposes)
-//
-// Binops is the class befriending the arithmetic operation classes
-// Derived is the class that's derived from Binops. Derived wants to offer +
-// and - operators. It does so by deriving from Binops<Derived, '+', '-'>
-//
-// The design is based on the CRTP, stepping over several classes
-// Structure (read the ascii art below from bottom to top):
-//
-//
-//   Binops0<Derived>            Sub<Derived>
-//          |   Ends the            |   Derived now also is a Sub
-//          |   recursion           |
-//          |                       |
-//          +---+-------------------+
-//              |
-//       Binops0<Derived, '-'>               Add<Derived>
-//              |   Recursively defning         |   Derived now also is an Add
-//              |   Binops0s for remaining      |
-//              |   operators                   |
-//              |                               |
-//              +-----------+-------------------+
-//                          |
-//                       Binops0<Derived, '+', '-'>
-//                          |   this forwards the Derived class to the
-//                          |   binary operator classes
-//                          |
-//                       Binops<Derived, '+', '-'>
-//                          |   declares all binary operator classes as friend
-//                          |
-//                        Derived
-//                            (wants, e.g., + and - operations)
-//
-//////////////////////////////////////////////////////////////////
-    // The basic arithmetic classes:
-
+#include <istream>
+#include <ostream>
 #include <utility>
 
-template <class Derived>
-struct Add
+    // The complete interface of the class template Binops: if additional
+    // operators are overloaded Binops requires no modifications.
+    //
+template <typename Derived>
+class Binops
 {
-    Derived &operator+=(Derived const &rhs) &; // Used as: object1 += object2
-    Derived &&operator+=(Derived const &rhs) &&;    // (anon.) += object2
+    template <typename Der>
+    friend std::ostream &operator<<(
+                            std::ostream &out, Binops<Der> const &rhs);
+
+    template <typename Der>
+    friend std::istream &operator>>(std::istream &in, Binops<Der> &rhs);
+    
+    void eWrap(std::istream &in);
+    void iWrap(std::ostream &out) const;
 };
 
-template <class Derived>
-Derived &Add<Derived>::operator+=(Derived const &rhs) &
+    // Completing the overloads for operator+: all function templates, that
+    // depend on operator+=(...) defined in Derived
+    //
+template <class Derived, typename Rhs>
+Derived &operator+=(Binops<Derived> &lhs, Rhs const &rhs) 
 {
-    Derived tmp{static_cast<Derived &>(*this)};
-    tmp.addWrap(rhs);
-    static_cast<Derived &>(*this).swap(tmp);
-    return static_cast<Derived &>(*this);
+    Derived tmp{ Derived{static_cast<Derived &>(lhs)} += rhs };
+    tmp.swap(static_cast<Derived &>(lhs));
+    return static_cast<Derived &>(lhs);
 }
 
-template <class Derived>
-Derived &&Add<Derived>::operator+=(Derived const &rhs) &&
+template <class Derived, typename Rhs>
+Derived operator+(Binops<Derived> &&lhs, Rhs const &rhs)
 {
-    static_cast<Derived &>(*this).addWrap(rhs);
-    return std::move(static_cast<Derived &>(*this));
+    return static_cast<Derived &&>(std::move(lhs)) += rhs;
 }
 
-template <class Derived>
-Derived operator+(Derived const &lhs, Derived const &rhs)
+template <class Derived, typename Rhs>
+Derived operator+(Binops<Derived> const &lhs, Rhs const &rhs)
 {
-    Derived ret{lhs};
-    ret.addWrap(rhs);
-    return ret;
+    return Derived{static_cast<Derived const &>(lhs)} += rhs;
 }
 
-template <class Derived>
-Derived operator+(Derived &&lhs, Derived const &rhs)
+    // Other operators may be added here, and are implemented analogously, see
+    // the Annotations.
+
+    // Implementing the insertion operator. The extraction operator is
+    // implemented analogously, see the Annotations:
+    //
+template <typename Derived>
+inline void Binops<Derived>::iWrap(std::ostream &out) const
 {
-    lhs.addWrap(rhs);
-    return std::move(lhs);
+    static_cast<Derived const &>(*this).insert(out);
 }
 
-////////////////////////////////////////////////////
-
-// Sub and Mul are provided as stubs. They can be implemented like Add.
-
-template <class Derived>
-struct Sub
+template <typename Derived>
+inline std::ostream &operator<<(std::ostream &out, Binops<Derived> const &rhs)
 {
-    void operator-=(Derived const &rhs)
-    {
-        static_cast<Derived &>(*this).subWrap(rhs);
-    }
-};
-
-template <class Derived>
-struct Mul
-{
-    void operator*=(Derived const &rhs)
-    {
-        static_cast<Derived &>(*this).mulWrap(rhs);
-    }
-};
-
-//////////////////////////////////////////////////////////////////
-    // The recursively defined Binops0 class: receives Derived, so a static
-    // cast becomes available to reach the basic arithmetic classes
-
-    // The general declaration:
-    //
-template <class Derived, int ...ops>
-class Binops0;
-
-    // The terminating definition:
-    //
-template <class Derived>
-class Binops0<Derived>
-{};
-
-    // The recursive definition: each variant inherits from the corresponding
-    // arithmetic class, and from itself, derived with the remaining operators
-    // (ops)
-    //
-    // This one is for addition:
-    //
-template <class Derived, int ...ops>
-class Binops0<Derived, '+', ops...>
-:
-    public Binops0<Derived, ops...>,
-    public Add<Derived>
-{};
-
-    // This one is for subtraction:
-    //
-template <class Derived, int ...ops>
-class Binops0<Derived, '-', ops...>
-:
-    public Binops0<Derived, ops...>,
-    public Sub<Derived>
-{};
-
-    // This one is for multiplication:
-    //
-template <class Derived, int ...ops>
-class Binops0<Derived, '*', ops...>
-:
-    public Binops0<Derived, ops...>,
-    public Mul<Derived>
-{};
-
-    // The class Binops befriends all arithmetic classes, and implements
-    // wrappers for the arithmetic functions potentially defined in
-    // Derived.
-    // The arithmetic classes call Binops' functions, which in turn call
-    // Derived's functions. This way Derived only needs to implement the
-    // necessary functions. Since the other functions aren't called, they
-    // arent't instantiated so there are no linker-errors
-    //
-template <class Derived, int ...ops>
-class Binops:
-    public Binops0<Derived, ops...>
-{
-    friend class Add<Derived>;
-    friend class Sub<Derived>;
-    friend class Mul<Derived>;
-
-            // instantiation declarations to make the free operators
-            // available:
-    friend Derived operator+<Derived>(Derived const &lhs, Derived const &rhs);
-    friend Derived operator+<Derived>(Derived &&lhs, Derived const &rhs);
-
-            // Note that the following function names should be different from
-            // the function names in Derived: if they're identical, and the
-            // corresponding Derived class function doesn't exist, then the
-            // program will recursively call Binops member, causing a
-            // stack-overflow, and therefore a segfault. I added B to indicate
-            // a Binops function.
-        void addWrap(Derived const &rhs);
-        void subWrap(Derived const &rhs);
-        void mulWrap(Derived const &rhs);
-};
-
-template <class Derived, int ...ops>
-inline void Binops<Derived, ops...>::addWrap(Derived const &rhs)
-{
-    static_cast<Derived &>(*this).add(rhs);
+    rhs.iWrap(out);
+    return out;
 }
 
-template <class Derived, int ...ops>
-inline void Binops<Derived, ops...>::subWrap(Derived const &rhs)
-{
-    static_cast<Derived &>(*this).sub(rhs);
-}
 
-template <class Derived, int ...ops>
-inline void Binops<Derived, ops...>::mulWrap(Derived const &rhs)
-{
-    static_cast<Derived &>(*this).mul(rhs);
-}
+
+
+
 
 ////////////////////////////////////////////////////////////////////////
 
     // User area: the user defines a class Derived, and wants to provide
-    // addition and subtraction.
-    // When additional operators are added to the template argument list
-    // (e.g. '*') compilation still succeeds, but because of the missing mul
-    // function compilation fails if associated operators are actually
-    // used (like o1 *= o2, shown below)
-    //
+    // addition and insertion: the operator+=() && and insert() functions have
+    // to be implemented and Derived derives from Binops<Derived>. For insert
+    // a friend declaration is also required.
 
 #include <iostream>
 
 using namespace std;
 
-class Derived: public Binops<Derived, '+', '-'>
+class Derived: public Binops<Derived>
 {
-    friend class Binops<Derived, '+', '-'>;
+    friend Binops<Derived>; // only required for stream insertion/extraction
 
     int d_value = 0;
 
@@ -224,36 +93,37 @@ class Derived: public Binops<Derived, '+', '-'>
 
         void swap(Derived &other);
 
+        Derived &&operator+=(Derived const &rhs) &&;
+
     private:
-        void add(Derived const &rhs);
-        void sub(Derived const &rhs);
+        void insert(std::ostream &out) const;
 };
+
+inline void Derived::insert(std::ostream &out) const
+{
+    out << "Inserting a Derived object";
+}
+
+Derived &&Derived::operator+=(Derived const &rhs) &&
+{
+    d_value += rhs.d_value;
+    return std::move(*this);
+}
 
 inline void Derived::swap(Derived &rhs)
 {
-    cout << "swapping " << d_value << " <-> " << rhs.d_value << "\n";
     std::swap(d_value, rhs.d_value);
-}
-
-inline void Derived::add(Derived const &rhs)
-{
-    cout << "adding " << d_value << " + " << rhs.d_value << "\n";
-    d_value += rhs.d_value;
-}
-
-inline void Derived::sub(Derived const &rhs)
-{
-    cout << "subtracting " << d_value << " - " << rhs.d_value << "\n";
-    d_value -= rhs.d_value;
 }
 
 int main()
 {
     Derived o1(1), o2(2);
     o1 += o2;
-    o1 -= o2;
-    // o1 *= o2;            adding this statement causes a compilation error
+    // o1 *= o2;            // adding this statement results in 
+                            // a compilation error
 
     o1 = o1 + o2;
     o1 += o1;
+
+    cout << o1 << '\n';
 }
