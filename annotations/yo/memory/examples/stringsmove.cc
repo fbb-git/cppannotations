dc46280a6d46cf4cdcd21435d8a8bee7173c34bb
Frank B. Brokken <f.b.brokken@rug.nl>
2017-05-22 19:19:06 +0200

    memory.yo: wip

--- annotations/yo/memory/examples/stringsmove.cc
+++ annotations/yo/memory/examples/stringsmove.cc
@@ -1,8 +1,8 @@
     Strings::Strings(Strings &&tmp)
     :
-        d_memory(tmp.d_memory),
+        d_string(tmp.d_string),
         d_size(tmp.d_size),
         d_capacity(tmp.d_capacity)
     {
-        tmp.d_memory = 0;
+        tmp.d_string = 0;
     }
