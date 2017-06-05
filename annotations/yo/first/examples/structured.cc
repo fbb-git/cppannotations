34f9ad3e372311078ec46359b7800401adf8e062
Frank B. Brokken <f.b.brokken@rug.nl>
2017-06-05 12:32:42 +0200

    covering structured binding declarations

---
+++ annotations/yo/first/examples/structured.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Return
+{
+    int first;
+    double second;
+};
+
+Return fun()
+{
+    return Return{1, 12.5};
+}
+
+Return &fun2()
+{
+    static Return ret{4, 5};
+    return ret;
+}
+
+int main()
+{
+    auto r1 = fun();
+
+    cout << r1.first;
+
+    auto [one, two] = fun();
+
+    auto &&[rone, rtwo] = fun();
+
+    auto &[lone, ltwo] = fun2();
+}    
