b6849d537d357c572a031b42f4a6bb153155d5d4
Frank B. Brokken <f.b.brokken@rug.nl>
2017-11-18 16:32:19 +0100

    wip covering free functions

---
+++ annotations/yo/stl/filesystem/examples/path.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <experimental/filesystem>
+
+// explicitly link against stdc++fs:
+//
+//      gx path.cc -lstdc++fs
+
+namespace fs = std::experimental::filesystem;
+
+int main()
+{
+    fs::path p{ "/abs/olu/te.cc" };
+
+
+    p.replace_filename("this/part");
+
+    std::cout << "Current path is " << fs::current_path() << '\n' << 
+                 "Absolute path for " << p << " is " << 
+                                                fs::absolute(p) << '\n';
+}
