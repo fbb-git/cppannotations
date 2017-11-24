b9bb9e9afe76377fa6dae69e8f1305b373c41b93
Frank B. Brokken <f.b.brokken@rug.nl>
2017-11-24 20:48:41 +0100

    using unordered_map in adv.templ./errcodeenum

--- annotations/yo/advancedtemplates/examples/errcodeenum.cc
+++ annotations/yo/advancedtemplates/examples/errcodeenum.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <unordered_map>
 #include <tuple>
 #include <system_error>
 
@@ -9,6 +9,7 @@ using namespace std;
     enum class CalculatorErrc
     {
         // no 0, since that's by convention implies no error
+        //  values do not have to be consecutive
         NoLvalue = 1,           // lhs of assignment is lvalue
         TypeError,              // incorrect expression type
         RangeError,             // e.g. sqrt(-1)
@@ -26,7 +27,7 @@ namespace std
 
 namespace 
 {
-    tuple<CalculatorErrc, char const *> s_errors[] =
+    unordered_map<CalculatorErrc, char const *> s_errors =
     {
         { CalculatorErrc::NoLvalue, "lhs of assignment is not a variable" },
         { CalculatorErrc::TypeError, "type of expression incorrect"       },
@@ -34,7 +35,6 @@ namespace
         { CalculatorErrc::ArityError, "incorrect number of arguments"     },
         { CalculatorErrc::UnknownFunction, "function not defined"         },
         { CalculatorErrc::MissingParentheses, "parentheses don't match"   },
-        { CalculatorErrc{}, "unrecognized CalculatorErrc"                 }
     };
 
     struct CalculatorErrCategory: public std::error_category
@@ -50,18 +50,12 @@ namespace
     
     string CalculatorErrCategory::message(int ce) const
     {
-        CalculatorErrc code = static_cast<CalculatorErrc>(ce);
-    
-        get<0>(s_errors[size(s_errors) - 1]) = code;
-    
-        return get<1>(
-                    *find_if(s_errors, s_errors + size(s_errors), 
-                        [=](auto const &entry)
-                        {
-                            return code == get<0>(entry);
-                        }
-                    )
-                );
+        auto iter = s_errors.find(static_cast<CalculatorErrc>(ce));
+
+        return iter == s_errors.end() ?
+                    "CalculatorErrc not recognized"
+                :
+                    iter->second;
     }
 
                                     // define the singleton
@@ -79,7 +73,7 @@ try
 {
     error_code ec = CalculatorErrc::ArityError;
     cout << ec << ' ' << ec.message() << '\n';
-    throw system_error{ ec, "Oops, something went wrong: " };
+    throw system_error{ ec, "For demonstration purposes: " };
 }
 catch (system_error &se)
 {
