78b4a0d353d2f6b444bd22120e30e28014c18bd3
Frank B. Brokken <f.b.brokken@rug.nl>
2017-12-02 14:38:48 +0100

    wip

--- annotations/yo/advancedtemplates/examples/errcode/errorcondition/makeerrrorcondition.cc
+++ annotations/yo/advancedtemplates/examples/errcode/errorcondition/makeerrrorcondition.cc
@@ -1,6 +1,8 @@
 #include "errorcondition.ih"
 
+//make
 std::error_condition make_error_condition(ErrorCondition::Enum ec)
 {
     return { static_cast<int>(ec), ConditionCategory::instance() };
 }
+//=
