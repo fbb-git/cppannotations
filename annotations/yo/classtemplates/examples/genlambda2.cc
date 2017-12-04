c81569e69eb5d5f841f53faa1e48482a4a36429a
Frank B. Brokken <f.b.brokken@rug.nl>
2017-12-04 23:15:07 +0100

    wip generic lambda expressions handling

---
+++ annotations/yo/classtemplates/examples/genlambda2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//code
+auto accumulate(auto const &container, auto function)
+{
+    auto accu = decltype(container[0]){};
+
+    for (auto &value: container)
+        accu = function(accu, value);
+
+    return accu;
+}
+
+auto lambda = [](auto lhs, auto rhs)
+            {
+                return lhs + rhs;
+            };
+
+int main()
+{
+    vector<int> values  = {1, 2, 3, 4, 5};
+    vector<string> text = {"a", "b", "c", "d", "e"};
+
+    cout << accumulate(values,  lambda) << '\n' << 
+            accumulate(text,    lambda) << '\n';
+}
+//=
